b0af8eeeb9fffc036fbcd75f85ddd392412465ad
Thazo <arenasgarrocarlos@gmail.com>
2025-05-06 00:11:00 -0500

    asdasdadasd

--- Matriculas/Controllers/ClaseController.cs
+++ Matriculas/Controllers/ClaseController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MatriculasMODELS;
 
 namespace Matriculas.Controllers
 {
     public class ClaseController : Controller
     {
-
         Uri direccion = new Uri("https://localhost:7117/Clase/");
         private readonly HttpClient httpClient;
 
@@ -13,8 +16,22 @@ namespace Matriculas.Controllers
             httpClient = new HttpClient();
             httpClient.BaseAddress = direccion;
         }
-        public IActionResult seleccionCarrera()
-        {|
+
+        public async Task<IActionResult> seleccionCarrera()
+        {
+            var response = await httpClient.GetAsync("Periodo/ObtenerPeriodoActual");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var periodo = JsonConvert.DeserializeObject<Periodo>(content);
+                ViewBag.CodigoPeriodoActual = periodo?.codigo_periodo; 
+            }
+            else
+            {
+                ViewBag.CodigoPeriodoActual = null;
+            }
+
             return View();
         }
     }
--- MatriculasMODELS/obj/Debug/net8.0/MatriculasMODELS.AssemblyInfo.cs
+++ MatriculasMODELS/obj/Debug/net8.0/MatriculasMODELS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatriculasMODELS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65dcd5c7c5b4f3e9630295d715bae64486b72d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42b48a88547ad6d147228342da54f61cf606411")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatriculasMODELS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatriculasMODELS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
